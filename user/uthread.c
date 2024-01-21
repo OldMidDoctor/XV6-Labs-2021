4cc93152c721d5f565bf36179c1008b636d7c68b
OldMidDoctor <g2092243894@163.com>
2024-01-21 15:52:17 +0800

    1.copy swtch code to  thread_switch to save and restore the registers 2.add struct thread_context add clear_thread to creat thread Invoke thread_switch to switch from t to next_thread

--- user/uthread.c
+++ user/uthread.c
@@ -10,8 +10,28 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for user-level thread switching
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
+  struct thread_context thread_context;    /* register status */
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 };
@@ -62,8 +82,20 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->thread_context, &current_thread->thread_context);
   } else
     next_thread = 0;
+}
+
+/*
+ * helper function to setup the routine for a newly-created thread
+ */
+void clear_thread(struct thread *t, void (*func)()) {
+  memset((void *)&t->stack, 0, STACK_SIZE);
+  memset((void *)&t->thread_context, 0, sizeof(struct thread_context));
+  t->state = RUNNABLE;
+  t->thread_context.sp = (uint64) ((char *)&t->stack + STACK_SIZE);  // 初始sp在栈顶
+  t->thread_context.ra = (uint64) func;  // 初始跳转位置是user传进来的线程函数
 }
 
 void 
@@ -74,8 +106,9 @@ thread_create(void (*func)())
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
-  t->state = RUNNABLE;
+  // t->state = RUNNABLE;
   // YOUR CODE HERE
+  clear_thread(t, func);
 }
 
 void 
