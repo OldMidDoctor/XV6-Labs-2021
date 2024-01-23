1fbe2c12ee30d939b51c70bd83755da40e7ce690
OldMidDoctor <g2092243894@163.com>
2024-01-23 10:07:38 +0800

    Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t locks[NBUCKET];    //// declare a lock
 
 
 double
@@ -40,6 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(&locks[i]);       // acquire lock
 
   // is the key already present?
   struct entry *e = 0;
@@ -54,20 +56,20 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&locks[i]);     // release lock
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&locks[i]);       // acquire lock
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&locks[i]);     // release lock
   return e;
 }
 
@@ -101,6 +103,11 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+  // initialize the lock
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL);
+  }
+
   pthread_t *tha;
   void *value;
   double t1, t0;
